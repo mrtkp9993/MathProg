2bc775b1ec68026e9373de84b6a30bd0c5a324a0
Murat Koptur <mkoptur3@gmail.com>
2020-11-01 08:55:20 +0300

    Update information.py

--- python/information.py
+++ python/information.py
@@ -7,3 +7,7 @@ def shannon_entropy(x: np.ndarray) -> np.float:
 
 def kl_divergence(p: np.ndarray, q: np.ndarray) -> np.float:
     return np.float(np.sum(p * np.log(p / q)))
+
+
+def sn_ratio(s: np.ndarray) -> np.float:
+    return np.mean(s)**2 / np.std(s)**2
