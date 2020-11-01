bafe1ec8d713813ed741959666275c4dfae38f15
Murat Koptur <mkoptur3@gmail.com>
2020-11-01 19:01:25 +0300

    Update information.py

--- python/information.py
+++ python/information.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 def shannon_entropy(x: np.ndarray) -> np.float:
-    return -np.sum(x * np.log2(x))
+    return -np.nansum(np.where(x > 0, x * np.log2(x), 0))
 
 
 def kl_divergence(p: np.ndarray, q: np.ndarray) -> np.float:
-    return np.float(np.sum(p * np.log(p / q)))
+    return np.float(np.where(p != 0, np.nansum(p * np.log(p / q)), 0))
 
 
 def sn_ratio(s: np.ndarray) -> np.float:
